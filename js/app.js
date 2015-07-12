291684b3ee271533aafa864e698bc051b8232b5b
Shivi <spandey2122@gmail.com>
2015-07-12 13:28:24 -0400

    Back to the basics, trying to make the collection and view work..

--- js/app.js
+++ js/app.js
@@ -1,61 +1,81 @@
+
 $(function(){
 
-var movies = new MoviesCollection();
 
-})
+  // <--- Defining MovieModel --->
 
-// <--- Defining MovieModel --->
-
-var MovieModel = Backbone.Model.extend({
-  defaults: {
-    title: 'unknown',
+  var MovieModel = Backbone.Model.extend({
+   defaults: {
+    title: "",
     year: 1900
-  }
-});
+   }
+  });
 
-// <--- Defining MoviesCollection --->
 
-var MoviesCollection = Backbone.Collection.extend({
-    model: Movie,
-    url:"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=16&country=us&apikey=gaggg3wsjg3z4amcrfhr45z5",
-    parse: function(data) {
-      console.log(data.movies);
-      return data.movies;
+  // <--- Defining MoviesCollection --->
+
+  var MoviesCollection = Backbone.Collection.extend({
+      model: MovieModel
+  });
+
+  //<--- A Bunch of Movies --->
+
+  var movies = new MoviesCollection([
+        new MovieModel({title: "Titanic", year: 1997}),
+        new MovieModel({title: "Avatar", year: 209}),
+        new MovieModel({title: "The Avengers", year: 2012}),
+        new MovieModel({title: "Star Wars", year: 1983}),
+        new MovieModel({title: "The Dark Knight", year: 2008}),
+        new MovieModel({title: "Inception", year: 2010}),
+        new MovieModel({title: "The Hunger Games", year: 2012}),
+        new MovieModel({title: "Jurassic Park", year: 1993}),
+        new MovieModel({title: "Furous 7", year: 2015}),
+        new MovieModel({title: "Transformers", year: 2007}),
+        new MovieModel({title: "Gravity", year: 2013}),
+
+      ]);
+
+
+
+  // <--- Defining MovieModel View --->
+
+  var MovieView = Backbone.View.extend({
+    tagName: 'li',
+
+    intialize: function(){
+
+      this.listenTo(this.model, 'change', this.render);
     },
-    sync:function(method, model, options){
-      options.timeout = 10000;
-      options.dataType = "jsonp";
-      return Backbone.sync(method, model, options);
-    },
-    initialize: function(){
-       // console.log("TEST");
-       this.fetch();
-  } 
-});
-return MoviesCollection;
 
-// <--- Defining MoviesCollection View --->
+    render: function(){
 
-var MoviesView = Backbone.View.extend({
+      this.$el.html('<h3>'+this.model.get("title")+'</h3><hr/><h5>'+this.model.get("year")+"</h5>");
+      return this;
+    }
 
-  el: ".page",
-  render: function(){
-    var that = this;
-    var movies = new Movies();
-    movies.fetch({
-      success: function(Movies){
-        var template = _.template($("#movie-Template").html());
-        that.$el.html(template);
+  });
+
+  // <--- Defining Main App View --->
+
+  var App = Backbone.View.extend({
+      el: $("#main"),
+
+      initialize: function(){
+        this.moviesList = $("#movies");
+        this.listenTo(movies, 'change', this.render);
+        movies.each(function(movie){
+          var view = new MovieView({model:movie});
+          this.moviesList.append(view.render().el); 
+        }, this);
+      },
+
+      render: function(){
+        _.each(movies, function(elem){
+          
+        });
       }
-    })
-  }
-  
-});
- 
-var view = new MoviesView();
-
-// <--- Defining MovieModel View --->
-
-var MovieView = Backbone.View.extend({
+  });
 
 });
+
+
