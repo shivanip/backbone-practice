bdd5488eac2ff6832c9209a100818f081eea4069
Shivi <spandey2122@gmail.com>
2015-07-12 18:03:56 -0400

    Got the initial movie page populated with backbone parsing of rottent tomatoes api

--- js/app.js
+++ js/app.js
@@ -1,81 +1,73 @@
-
 $(function(){
 
-
-  // <--- Defining MovieModel --->
-
-  var MovieModel = Backbone.Model.extend({
-   defaults: {
-    title: "",
-    year: 1900
-   }
+  var Movie = Backbone.Model.extend({
+    defaults:{
+      title: 'My movies',
+      year: 100,
+      posters:{thumbnail:""}
+    }
   });
 
+  var MovieList = Backbone.Collection.extend({
+    model: Movie,
+    url:"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=16&country=us&apikey=gaggg3wsjg3z4amcrfhr45z5",
+    parse: function(data) {
+      // console.log(data.movies);
+      // var movies = data.movies;
+      // var cleanMovies = [];
+      // Object.keys(movies).forEach(function(key){ 
+      //   var value = movies[key];
+      //   cleanMovies.push(value);
+      //   // console.log(value.title+","+value.year);
+      //   // cleanMovies.push({title:value.title,posters:value.posters.thumbnail,year:value.year});
 
-  // <--- Defining MoviesCollection --->
+      // });
+      console.log(data.movies);
+      return data.movies;
+    },
+    sync:function(method, model, options){
+      options.timeout = 10000;
+      options.dataType = "jsonp";
+      return Backbone.sync(method, model, options);
+    },
+    initialize: function(){
+       console.log("Cllection initialized");
+    } 
 
-  var MoviesCollection = Backbone.Collection.extend({
-      model: MovieModel
   });
-
-  //<--- A Bunch of Movies --->
-
-  var movies = new MoviesCollection([
-        new MovieModel({title: "Titanic", year: 1997}),
-        new MovieModel({title: "Avatar", year: 209}),
-        new MovieModel({title: "The Avengers", year: 2012}),
-        new MovieModel({title: "Star Wars", year: 1983}),
-        new MovieModel({title: "The Dark Knight", year: 2008}),
-        new MovieModel({title: "Inception", year: 2010}),
-        new MovieModel({title: "The Hunger Games", year: 2012}),
-        new MovieModel({title: "Jurassic Park", year: 1993}),
-        new MovieModel({title: "Furous 7", year: 2015}),
-        new MovieModel({title: "Transformers", year: 2007}),
-        new MovieModel({title: "Gravity", year: 2013}),
-
-      ]);
-
-
-
-  // <--- Defining MovieModel View --->
 
   var MovieView = Backbone.View.extend({
-    tagName: 'li',
-
-    intialize: function(){
-
+    tagName: 'div',
+    className:'col-md-1',
+    initialize: function(){
       this.listenTo(this.model, 'change', this.render);
     },
-
     render: function(){
-
-      this.$el.html('<h3>'+this.model.get("title")+'</h3><hr/><h5>'+this.model.get("year")+"</h5>");
+      this.$el.html('<img src="' + this.model.get('posters').thumbnail+ '" /> ' + '<figcaption>'+this.model.get('title')+'</figcaption>' + '<span>' + this.model.get('year') + '</span>');
       return this;
     }
-
   });
 
-  // <--- Defining Main App View --->
-
-  var App = Backbone.View.extend({
-      el: $("#main"),
-
-      initialize: function(){
-        this.moviesList = $("#movies");
-        this.listenTo(movies, 'change', this.render);
-        movies.each(function(movie){
-          var view = new MovieView({model:movie});
-          this.moviesList.append(view.render().el); 
-        }, this);
-      },
-
-      render: function(){
-        _.each(movies, function(elem){
-          
-        });
-      }
+  var MoviesView = Backbone.View.extend({
+    tagName:'li',
+    className:'row',
+    el: $('#movies'),
+    initialize: function(){        
+      this.collection = new MovieList();      
+      this.collection.on("sync", this.render, this);
+      this.collection.fetch();      
+    },
+    render: function(){
+      console.log("called render");
+      this.collection.each(function(movie){
+        console.log(movie.year);
+        var view = new MovieView({ model: movie });
+        this.$el.append(view.render().el);
+      }, this);
+      return this;
+    }
   });
+
+  new MoviesView();
 
 });
-
-
