967b6366079d4a7b0203a2f262a7337fde108769
Shivi <spandey2122@gmail.com>
2015-07-13 08:41:27 -0400

    Added synopsis and style to the view

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 $(function(){
 
   var Movie = Backbone.Model.extend({
@@ -12,16 +13,6 @@ $(function(){
     model: Movie,
     url:"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=16&country=us&apikey=gaggg3wsjg3z4amcrfhr45z5",
     parse: function(data) {
-      // console.log(data.movies);
-      // var movies = data.movies;
-      // var cleanMovies = [];
-      // Object.keys(movies).forEach(function(key){ 
-      //   var value = movies[key];
-      //   cleanMovies.push(value);
-      //   // console.log(value.title+","+value.year);
-      //   // cleanMovies.push({title:value.title,posters:value.posters.thumbnail,year:value.year});
-
-      // });
       console.log(data.movies);
       return data.movies;
     },
@@ -31,20 +22,22 @@ $(function(){
       return Backbone.sync(method, model, options);
     },
     initialize: function(){
-       console.log("Cllection initialized");
+       console.log("Collection initialized");
     } 
 
   });
 
   var MovieView = Backbone.View.extend({
-    tagName: 'div',
-    className:'col-md-1',
+    tagName: 'ul',
+    //className:'col-md-1',
     initialize: function(){
-      this.listenTo(this.model, 'change', this.render);
+      // this.listenTo(this.model, 'change', this.render);
     },
     render: function(){
-      this.$el.html('<img src="' + this.model.get('posters').thumbnail+ '" /> ' + '<figcaption>'+this.model.get('title')+'</figcaption>' + '<span>' + this.model.get('year') + '</span>');
-      return this;
+
+      this.$el.html('<div class="row"><div class="col-xs-6 col-md-3"><a href="#" class="thumbnail"><img src="' + this.model.get('posters').thumbnail+ '" /> </a></div>' + '<figcaption>'+this.model.get('title')+'</figcaption>' + '<span>' + this.model.get('year')+'</span><hr/><span>'+this.model.get('synopsis') +'</span></div>');
+      // console.log(this);
+      return this; // It's good practice for chaining.
     }
   });
 
@@ -58,9 +51,9 @@ $(function(){
       this.collection.fetch();      
     },
     render: function(){
-      console.log("called render");
       this.collection.each(function(movie){
-        console.log(movie.year);
+        // At this stage the movie object is not populated so logging attribute normally will not work
+        // instead you will have to extract from attributes property
         var view = new MovieView({ model: movie });
         this.$el.append(view.render().el);
       }, this);
